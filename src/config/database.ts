4cf4debc54c057b168befd2011c722669f7ff537
Abhishek Kumar Gupta <abhishekkumargupta7860@gmail.com>
2025-01-13 18:44:44 +0530

    comitted

--- src/config/database.ts
+++ src/config/database.ts
@@ -18,7 +18,7 @@ export const connectDB = async () => {
         console.log('MySQL connected');
 
         // Sync all models
-        await sequelize.sync({ force: false }); // Set force to true to drop and recreate tables
+        await sequelize.sync({ force: true }); // Set force to true to drop and recreate tables
         console.log('All models were synchronized successfully.');
     } catch (error) {
         console.error('MySQL connection error:', error);
