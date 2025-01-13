187da3018702e760136ea78dd7ec1a2975bb8403
Abhishek Kumar Gupta <abhishekkumargupta7860@gmail.com>
2025-01-13 22:34:54 +0530

    update db configs

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,6 +9,7 @@ const sequelize = new Sequelize({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT || '3306'),
 });
 
 export const connectDB = async () => {
